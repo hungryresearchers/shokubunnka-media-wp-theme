40718969b5133b776cd6807f8d72cc288655327f
Ryuichi Sakagami <shagamiiiii@gmail.com>
2018-10-01 17:40:35 +0900

    imple cross-origin

--- functions.php
+++ functions.php
@@ -183,3 +183,18 @@ function wp_rest_api_post() {
 }
 add_action( 'rest_api_init', 'wp_rest_api_post');
 
+/**
+ * Use * for origin
+ */
+add_action( 'rest_api_init', function() {
+    
+	remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+	add_filter( 'rest_pre_serve_request', function( $value ) {
+		header( 'Access-Control-Allow-Origin: *' );
+		header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE' );
+		header( 'Access-Control-Allow-Credentials: true' );
+
+		return $value;
+		
+	});
+}, 15 );
